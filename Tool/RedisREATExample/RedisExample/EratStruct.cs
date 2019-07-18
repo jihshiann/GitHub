e0934449487c7cdd56b0032671c370c676e44461
蔡季憲 <jixiantsai@lionmail.com>
2019-07-18 11:32:19 +0800

    Separate the  Repository

--- Tool/RedisREATExample/RedisExample/EratStruct.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Redis_LinuxServer
-{
-    public class EratStruct
-    {
-        public string Curr { get; set; }
-        public string Tcurr { get; set; }
-        public double Erat { get; set; }
-    }
-}
