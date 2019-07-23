5451c78d1ea81a6564021befd16de26c0c05d191
蔡季憲 <jixiantsai@lionmail.com>
2019-07-23 17:03:43 +0800

    Add .NET Core Pratice folder

---
+++ dotNetCorePratice/MvcMovie/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required]
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]//Is not working at html5 when type is date
+        [DataType(DataType.Date), Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]//Accept white space or null
+        public string Rating { get; set; }
+    }
+}
